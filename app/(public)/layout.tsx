aa0a64e180b1f1aa3dabe60d208ca1dee5cd5b70
Hugo <hugo.morales.pro@gmail.com>
2025-09-12 10:18:04 +0200

    fix: Retirer la double navbar dark du layout public

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,15 +1,7 @@
-import Navigation from "@/components/public/navigation";
-
 export default async function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="min-h-screen bg-black z-1 relative">
-      <div className="background__noisy" />
-      <Navigation />
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
